b24ac659fdfc5660ef58f7255b293a5d8be79101
Teddy-38 <Zhdanov_as94@mail.ru>
2025-02-05 21:33:18 +0800

    Task3

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,12 @@ public class Main {
         String lastName = " Ivanov";
         String fullName = (firstName + middleName + lastName);
         System.out.println(fullName);
-
-
-
-
+        System.out.println();
+        System.out.println("Task2");
+        System.out.println((firstName.toUpperCase() + middleName.toUpperCase() + lastName.toUpperCase()));
+        System.out.println("Task3");
+        String people = "Иванов Семён Семёнович";
+        String streetTrue = people.replace("ё", "е");
+        System.out.println(streetTrue);
     }
 }
